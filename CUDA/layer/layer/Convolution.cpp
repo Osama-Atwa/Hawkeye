38e0b4bcd7cd56949f938db27b9de679f1a4fc6c
Osama-Atwa <ashrafosama536@gmail.com>
2022-05-03 16:50:03 +0200

    Implement_convolve_execute
    implementing the execution of  convolve and the loading parameters function

--- CUDA/layer/layer/Convolution.cpp
+++ CUDA/layer/layer/Convolution.cpp
@@ -19,7 +19,26 @@ int Convolution::get_filters_h() { return filters_h; }
 
 void Convolution::load_parameters(vector<float>& V) {
 
+	int n_f = get_no_filters();
+	int f_w = get_filters_w();
+	int f_h = get_filters_h();
+	vector<float> F1;
+	vector<vector<float>> F2;
+	for (int i = 0; i < n_f; i++)
+	{
+		for (int j = 0; j < f_w; j++)
+		{
+			for (int l = 0; l < f_h; l++)
+			{
+				F1.push_back(V[i + j + l]);
+			}
+			F2.push_back(F1);
+			F1.clear();
+		}
+		weights.push_back(F2);
+		F2.clear();
+	}
 }
 void Convolution::execute(vector<float>& v_input,vector<float>& v_output) {
-
+	v_output = convolve2NN(v_input, weights);
 }
