296c20bc230f635b49e4cd61c724728a16f9df0f
Osama-Atwa <81799153+Osama-Atwa@users.noreply.github.com>
2022-05-03 13:21:14 +0200

    update array.cpp

--- CUDA/layer/layer/Array.cpp
+++ CUDA/layer/layer/Array.cpp
@@ -12,3 +12,63 @@ Array<T>::Array(vector<int> dimemsions)
 	data.resize(nelem);
 }
 
+template<typename T> Array<T>::Array(T arr[], int l)
+{
+    data = new T[l];
+    length = l;
+    for (int i - 0; i < length; i++)
+    {
+        data.push_back(arr[i]);
+    }
+}
+
+template<typename T> Array<T>::Array(T** arr, int dim0, int dim1)
+{
+    lenght = dim0 * dim1;
+    data = new T[length];
+
+    for (int i = 0; i < dim0; i++) {
+        for (int j = 0; j < dim1; j++)
+        {
+            data.push_back(arr[i][j]);
+
+        }
+    }
+}
+
+template<typename T> Array<T>::Array(T*** arr, int dim0, int dim1, int dim2)
+{
+    lenght = dim0 * dim1 * dim2;
+    data = new T[length];
+
+    for (int i = 0; i < dim0; i++) {
+        for (int j = 0; j < dim1; j++)
+        {
+            for (int k = 0k < dim2; k++)
+            {
+                data.push_back(arr[i][j]);
+
+            }
+        }
+    }
+}
+
+template<typename T> Array<T>::Array(T**** arr, int dim0, int dim1, int dim2, int dim3)
+{
+    lenght = dim0 * dim1 * dim2 * dim3;
+    data = new T[length];
+
+    for (int i = 0; i < dim0; i++) {
+        for (int j = 0; j < dim1; j++)
+        {
+            for (int k = 0k < dim2; k++)
+            {
+                for (int l = 0; l < dim3; l++)
+                {
+                    data.push_back(arr[i][j]);
+                }
+            }
+        }
+    }
+}
+
