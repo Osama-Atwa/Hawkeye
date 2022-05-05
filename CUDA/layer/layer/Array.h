c71b468d89e091926a8236dbe9ea470f7dd7f686
Osama-Atwa <ashrafosama536@gmail.com>
2022-05-05 12:06:33 +0200

    update Array.h
    calculating the index from 3 indexes

--- CUDA/layer/layer/Array.h
+++ CUDA/layer/layer/Array.h
@@ -13,9 +13,86 @@ private:
     vector<int> dim;
 public:
     Array(vector<int> dimemsions);
-    Array(T arr[], int l);
+    void fill_data(vector<T> arr);
     Array(T** arr, int dim0, int dim1);
     Array(T*** arr, int dim0, int dim1, int dim2);
     Array(T**** arr, int dim0, int dim1, int dim2, int dim3);
+    T operator()(int i0, int i1 = -1, int i2 = -1, int i3 = -1);
 };
 
+template<typename T>
+Array<T>::Array(vector<int> dimemsions)
+{
+    dim = dimemsions;
+    int nelem = 0;
+    for (size_t i = 0; i < dimemsions.size(); i++)
+    {
+        nelem += dimemsions[i];
+    }
+    data.resize(nelem);
+}
+
+template<typename T> void Array<T>::fill_data(vector<T> arr)
+{
+    data = arr;
+}
+
+template<typename T> Array<T>::Array(T** arr, int dim0, int dim1)
+{
+   
+}
+
+template<typename T> Array<T>::Array(T*** arr, int dim0, int dim1, int dim2)
+{
+
+    for (int i = 0; i < dim0; i++) {
+        for (int j = 0; j < dim1; j++)
+        {
+            for (int k = 0; k < dim2; k++)
+            {
+                data.push_back(arr[i][j][k]);
+
+            }
+        }
+    }
+}
+
+template<typename T> Array<T>::Array(T**** arr, int dim0, int dim1, int dim2, int dim3)
+{
+
+    for (int i = 0; i < dim0; i++) {
+        for (int j = 0; j < dim1; j++)
+        {
+            for (int k = 0; k < dim2; k++)
+            {
+                for (int l = 0; l < dim3; l++)
+                {
+                    data.push_back(arr[i][j][k][l]);
+                }
+            }
+        }
+    }
+}
+
+template<typename T> T Array<T>::operator()(int i0, int i1, int i2, int i3)
+{
+    if (i1 == -1)
+        return data[i0];
+    else if (i2 == -1)
+    {
+        int x = i0* dim[1] + i1 ;
+        return data[x];
+    }
+    else if (i3 == -1 && i2 != -1)
+    {
+        int x = i0 * (dim[0] * dim[1]) + i1 * dim[1] + i2;
+        return data[x];
+
+    }
+    else if (i3 != -1)
+    {
+        int x = i0 * (dim[0] * dim[1] * dim[2]) + i1 * (dim[0] * dim[1]) + i2 * dim[2] + i3;
+        return data[x];
+    }
+        
+}
