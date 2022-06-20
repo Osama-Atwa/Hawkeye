96f81ef5b64d9e370fe5c6304b4baa13a28b5470
Fat7y99 <67173807+Fat7y99@users.noreply.github.com>
2022-06-20 16:58:41 +0200

    Update avg_pool_single_tb.sv

--- Verilog/AvgPool/SingleChannel/avg_pool_single_tb.sv
+++ Verilog/AvgPool/SingleChannel/avg_pool_single_tb.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/15/2020 12:46:19 AM
-// Design Name: 
-// Module Name: avg_pool_single_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module avg_pool_single_tb;
 
